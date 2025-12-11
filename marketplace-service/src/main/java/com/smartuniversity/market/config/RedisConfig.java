e6cb75e8f61b4033b320ce17556d1b292f1eee62
Name <amir0480048012@gmail.com>
2025-12-11 16:51:14 +0000

    sdgfnd

--- marketplace-service/src/main/java/com/smartuniversity/market/config/RedisConfig.java
+++ marketplace-service/src/main/java/com/smartuniversity/market/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -25,8 +26,11 @@ import java.util.Map;
  * 
  * Cache Names:
  * - productsByTenant: Caches product listings per tenant (10 min TTL)
+ * 
+ * Note: This config is disabled during tests (profile "!test") to use simple in-memory cache instead.
  */
 @Configuration
+@Profile("!test")
 public class RedisConfig implements CachingConfigurer {
 
     @Bean
