798dc6e7c2188649c8717c2f13d2068d3da25a92
sismondiosa-svg <sismondiosa@gmail.com>
2025-12-22 08:18:45 +0330

    Fix #3: Implement close() method in ScheduledExamState

--- exam-service/src/main/java/com/smartuniversity/exam/state/ScheduledExamState.java
+++ exam-service/src/main/java/com/smartuniversity/exam/state/ScheduledExamState.java
@@ -2,6 +2,8 @@ package com.smartuniversity.exam.state;
 
 import com.smartuniversity.exam.domain.Exam;
 import com.smartuniversity.exam.domain.ExamStateType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * SCHEDULED exam can be started and transitions to LIVE.
@@ -16,6 +18,11 @@ public class ScheduledExamState implements ExamState {
     @Override
     public void start(Exam exam) {
         exam.setState(ExamStateType.LIVE);
+    }
+
+    @Override
+    public void close(Exam exam) {
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "Cannot close a scheduled exam. Start it first.");
     }
 
     @Override
